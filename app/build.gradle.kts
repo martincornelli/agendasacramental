28c991ac7e29b00b764589ca107db28a3ba97e25
martincornelli <mcornellirodriguez@byupathway.edu>
2026-06-27 14:56:18 -0300

    Bump release version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.martincornelli.agendasacramental"
         minSdk = 24
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.2.3"
+        versionCode = 30
+        versionName = "1.2.5"
     }
 
     buildFeatures {
